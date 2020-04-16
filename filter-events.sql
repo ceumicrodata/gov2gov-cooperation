6a54594ae7e15cc645f0c9c67c5ff93ab5b6f237
Miklos Koren <miklos.koren@gmail.com>
2020-04-16 16:58:39 +0200

    Download broader set of events from GDELT

--- filter-events.sql
+++ filter-events.sql
@@ -1,7 +1,9 @@
 SELECT GLOBALEVENTID, SQLDATE, Actor1Code, Actor1CountryCode, Actor1Type1Code, Actor2Code, 
-Actor2CountryCode, Actor2Type1Code, EventBaseCode, NumMentions, ActionGeo_CountryCode, SOURCEURL
+Actor2CountryCode, Actor2Type1Code, EventBaseCode, EventRootCode, NumMentions, ActionGeo_CountryCode, SOURCEURL
   FROM `gdelt-bq.gdeltv2.events`
   WHERE Actor1CountryCode IN ('ALB','ARM','AUT','AZE','BIH','BEL','BGR','BLR','CHE','CYP','CZE','DEU','DNK','DZA','EST','EGY','ESP','FIN','FRA','GBR','GEO','GRC','HRV','HUN','IRL','ISR','ISL','ITA','JOR','LBN','LIE','LTU','LUX','LVA','LBY','MAR','MDA','MNE','MKD','MLT','NLD','NOR','POL','PSE','PRT','ROU','SRB','RUS','SWE','SVN','SVK','SYR','TUN','TUR','UKR')
+  AND Actor2CountryCode IS NOT NULL
+  AND NOT Actor2CountryCode = Actor1CountryCode
   AND Actor1Type1Code = 'GOV'
   AND Actor2Type1Code = 'GOV'
-  AND EventBaseCode IN ('042', '043', '044', '046', '057', '061')
+  AND EventRootCode IN ('03', '04', '05', '06')
